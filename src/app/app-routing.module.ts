47246786ee6e7040ee5e81769ad68568377e9d88
climaxhome <57805640+climaxhome@users.noreply.github.com>
2020-01-14 01:06:39 +0700

    Fresh start of input_id 1.00
    - change initial routing to input-id
    - create uid with student id
    - check input student id with the one in database

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,18 @@ import { AuthGuard } from '../providers/authguard.service';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'input-id', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
     path: 'freshcodes',
     loadChildren: () => import('./freshcodes/freshcodes.module').then( m => m.FreshcodesPageModule),
     canActivate: [AuthGuard]
   },
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminPageModule)}
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminPageModule)},
+  {
+    path: 'input-id',
+    loadChildren: () => import('./input-id/input-id.module').then( m => m.InputIdPageModule)
+  }
 ];
 
 @NgModule({
---
+++ src/app/input-id/input-id.page.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Component({
+  selector: 'app-input-id',
+  templateUrl: './input-id.page.html',
+  styleUrls: ['./input-id.page.scss'],
+})
+export class InputIdPage implements OnInit {
+
+  constructor(
+    public router: Router,
+    public alertController: AlertController,
+    public afs: AngularFirestore
+  ) {}
+
+  ngOnInit() {
+  }
+
+  id
+  id_string
+
+  inputid() {
+    this.id_string = this.id.toString()
+    if (this.id_string.length == 10) {
+      this.afs.collection(`users`).doc(this.id_string).get() //check with firestore database that this id have been created or not
+        .toPromise()
+        .then(doc => {
+          if(!doc.exists) {
+            console.log('Not register Student ID!');
+            this.afs.collection(`users`).doc(this.id_string).set({
+              StudentID : this.id
+            })
+            this.router.navigate([""]); //redirect to register page
+          } else {
+            // this.showAlert("Error", "ID Already Created!")
+            console.log('Document Data: ', doc.data());
+            this.router.navigate(["home"]);
+          }
+        })
+        .catch(err =>{
+          console.log('Error',err);
+        })
+        // console.log("hey")
+    }
+    else {
+      this.showAlert("Error", "Please input 10 Digit Student ID!")
+    }
+  }
+
+  // inputid() {
+  //   this.id_string = this.id.toString()
+  //   if (this.id_string.length == 10) {
+  //     if (this.afs.collection(`users`).doc(this.id_string).get()
+  //       .toPromise()
+  //       .then(doc => {
+  //         if(!doc.exists) {
+  //           console.log('No ID!');
+  //         } else {
+  //           console.log('Document Data: ', doc.data());
+  //         }
+  //       })
+  //       .catch(err =>{
+  //         console.log('Error',err);
+  //       })
+  //       ) { //to check with firestore database that this id have been created or not
+  //       this.showAlert("Error", "ID Already Created!")
+  //       this.router.navigate(["home"]);
+  //       console.log("hey")
+        
+  //     } 
+  //     else {
+  //       this.afs.collection(`users`).doc(this.id_string).set({
+  //         StudentID : this.id
+  //       })
+  //       this.router.navigate([""]); //redirect to register page
+  //     }
+  //   }
+  //   else {
+  //     this.showAlert("Error", "Please input 10 Digit Student ID!")
+  //   }
+  // }
+
+  async showAlert(header: string,message: string) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ["Ok"]
+    })
+    await alert.present()
+    let result = await alert.onDidDismiss();
+    console.log(result)
+  }
+}
